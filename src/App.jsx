676e07caa26e53abb4cb4e32f26760bf73bf7bb6
leeejin <asd72621@naver.com>
2024-05-30 17:30:33 +0900

    app.js 재수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes/router";
-function Loading() {
-  return (
-    <div>
-      <h3>Loading...</h3>
-    </div>
-  );
-}
+
 function App() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <RouterProvider router={router} />
-    </Suspense>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
